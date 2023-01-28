ed86e94fd1b907467400d1affcd4394390fb4259
atultiwaree <2010344.bca.cbsa@cgc.edu.in>
2023-01-29 02:26:51 +0530

    silly

--- helpers/msg.constants.js
+++ helpers/msg.constants.js
@@ -1,8 +1,10 @@
 module.exports.msgConstants = {
-  //Official
+  //Developer errors.
   nodeStarted: "Node started at port",
   mongoStarted: "Mongo started successfully",
   mongoError: "MongoDB connection error.",
   nodeError: "Node error",
+
+  //Clients errors.
   somethingWentWrong: "Sorry there was some error.",
 };
