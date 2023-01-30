ca55817eb9692e7317f25690150184d2289b51c9
atultiwaree <2010344.bca.cbsa@cgc.edu.in>
2023-01-30 17:56:20 +0530

    Added another function to utils file called timer whcich will convert given minutes from client to ISO Date format

--- helpers/utility.functions.js
+++ helpers/utility.functions.js
@@ -5,3 +5,9 @@ module.exports.create_success_response = (message, data, success = true) => {
 module.exports.create_failure_response = (message, data, success = false) => {
   return { success, message, data };
 };
+
+module.exports.set_timer = (minutes) => {
+  let tempDate = new Date();
+  let destoryDate = new Date(tempDate.setMinutes(tempDate.getMinutes() + minutes)).toISOString();
+  return destoryDate;
+};
